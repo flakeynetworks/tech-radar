8f31ae7a534cf35f7dd2aee967defd2623389628
richard.stokes <richard@flakeynetworks.co.uk>
2022-01-06 02:21:31 +0000

    INTERNAL: Improves the positioning of the points in the zones

--- javascript/radar.js
+++ javascript/radar.js
@@ -19,12 +19,29 @@ class Point extends RadarElement {
 
         return new Point(data, container);
     }
+
+    setPositionProvider(positionProvider) {
+        this.positionProvider = positionProvider;
+    }
+
+    setDepthProvider(depthProvider) {
+        this.depthProvider = depthProvider;
+    }
+
+    redraw() {
+        this.container.style.setProperty('--angle', this.positionProvider(this));
+        this.container.style.setProperty('--depth', this.depthProvider());
+    }
 }
 
 class Zone extends RadarElement {
 
     constructor(data, container) {
+
         super(data, container);
+        this.points = [];
+
+        this.redraw();
     }
 
     static build(zoneData, segmentData) {
@@ -32,10 +49,60 @@ class Zone extends RadarElement {
         let container = document.createElement("div");
         container.className = "radar_zone";
         container.style.setProperty('--bg', segmentData.background);
-        container.style.setProperty('--value', zoneData.value);
 
         // Create the references
         return new Zone(zoneData, container);
+    }
+
+    addPoint(point) {
+
+        this.points.push(point);
+
+        point.setPositionProvider(point => {
+
+            let index = 1;
+
+            for(let i = 0; i < this.points.length; i++) {
+                if(this.points[i] === point) {
+                    index = i + 1;
+                    break;
+                }
+            }
+
+            return (this.angleProvider() / (this.points.length + 1)) * index;
+        });
+
+        point.setDepthProvider(() => {
+
+            let zoneWidth = this.widthProvider(this);
+            let offset = 100 - zoneWidth;
+
+            let randomMin = 0.50;
+            let randomMax = 0.80;
+            let randomDepth = Math.random() * (randomMax - randomMin) + randomMin;
+
+            console.log("Random: " + randomDepth);
+            return zoneWidth * randomDepth + offset;
+        })
+
+        this.container.appendChild(point.container);
+        this.redraw();
+    }
+
+    redraw() {
+
+        this.container.style.setProperty('--value', this.data.value);
+        this.points.forEach(point => { point.redraw(); })
+    }
+
+    getWidth() { return Number(this.data.value.replaceAll("%", "")); }
+
+    setAngleProvider(provider) {
+        this.angleProvider = provider;
+    }
+
+    setWidthProvider(provider) {
+        this.widthProvider = provider;
     }
 }
 
@@ -56,12 +123,42 @@ class Segment extends RadarElement {
         let segment = new Segment(segmentData, container);
 
         data.zones.forEach(zoneData => {
-            let zone = Zone.build(zoneData, segmentData);
-            segment.zones.push(zone);
-            container.appendChild(zone.container);
+            segment.addZone(Zone.build(zoneData, segmentData));
         });
 
         return segment;
+    }
+
+    addZone(zone) {
+
+        zone.setAngleProvider(() => { return this.data["degrees"]; });
+
+        zone.setWidthProvider(zone => {
+            let position = this.zones.indexOf(zone);
+
+            if(this.zones.length !== position + 1) {
+
+                let innerZoneWidth = this.zones[position + 1].getWidth();
+                let zoneWidth = zone.getWidth();
+
+                return ((zoneWidth - innerZoneWidth) / zoneWidth) * 100;
+            }
+
+            return 100;
+        })
+
+        this.zones.push(zone);
+        this.container.appendChild(zone.container);
+    }
+
+    addPoint(point) {
+
+        this.zones.forEach(zone => {
+
+            if(zone.data.name === point.data["zone"]) {
+                zone.addPoint(point);
+            }
+        })
     }
 }
 
@@ -69,7 +166,6 @@ class Radar extends RadarElement {
 
     constructor(data, container) {
         super(data, container);
-
         this.segments = [];
     }
 
@@ -108,6 +204,11 @@ class Radar extends RadarElement {
         return radar;
     }
 
+    redraw() {
+
+        this.segments.forEach(segment => segment.redraw());
+    }
+
     addPoint(data) {
 
         let point = Point.build(data);
@@ -116,12 +217,7 @@ class Radar extends RadarElement {
 
             if(segment.data.name === data["segment"]) {
 
-                segment.zones.forEach(zone => {
-
-                    if(zone.data.name === data["zone"]) {
-                        zone.container.appendChild(point.container);
-                    }
-                })
+                segment.addPoint(point);
             }
         })
     }
